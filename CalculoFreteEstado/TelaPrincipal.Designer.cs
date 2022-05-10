270177de4f1f26903af6ea4196055acbfb762b46
GUILHERME SOARES DE ALMEIDA <guisoares.almeida8@gmail.com>
2022-05-10 08:58:49 -0300

    Adicionar arquivos de projeto.

---
+++ CalculoFreteEstado/TelaPrincipal.Designer.cs
@@ -0,0 +1,214 @@
+﻿
+namespace CalculoFreteEstado
+{
+    partial class TelaPrincipal
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.txtPorcento = new System.Windows.Forms.TextBox();
+            this.txtValor = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.cbEstado = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(126, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(273, 30);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Cálculo de Frete por Estado";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI Semibold", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(18, 105);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nome:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI Semibold", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(18, 139);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 25);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Valor:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI Semilight", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(10, 177);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(78, 25);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "%Frete:";
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(100, 110);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(214, 23);
+            this.txtNome.TabIndex = 4;
+            // 
+            // txtPorcento
+            // 
+            this.txtPorcento.Location = new System.Drawing.Point(101, 181);
+            this.txtPorcento.Name = "txtPorcento";
+            this.txtPorcento.ReadOnly = true;
+            this.txtPorcento.Size = new System.Drawing.Size(135, 23);
+            this.txtPorcento.TabIndex = 5;
+            // 
+            // txtValor
+            // 
+            this.txtValor.Location = new System.Drawing.Point(100, 144);
+            this.txtValor.Name = "txtValor";
+            this.txtValor.Size = new System.Drawing.Size(214, 23);
+            this.txtValor.TabIndex = 6;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.label5.Location = new System.Drawing.Point(357, 112);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(33, 21);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "UF:";
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Font = new System.Drawing.Font("Segoe UI", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnCalcular.Location = new System.Drawing.Point(373, 181);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(75, 23);
+            this.btnCalcular.TabIndex = 8;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label6.Location = new System.Drawing.Point(26, 251);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(63, 15);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Valor Final";
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.lblResultado.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblResultado.Location = new System.Drawing.Point(26, 280);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(475, 115);
+            this.lblResultado.TabIndex = 10;
+            this.lblResultado.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // cbEstado
+            // 
+            this.cbEstado.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
+            this.cbEstado.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
+            this.cbEstado.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbEstado.FormattingEnabled = true;
+            this.cbEstado.Items.AddRange(new object[] {
+            "Escolha um Estado",
+            "AM",
+            "MG",
+            "RJ",
+            "SP"});
+            this.cbEstado.Location = new System.Drawing.Point(396, 110);
+            this.cbEstado.Name = "cbEstado";
+            this.cbEstado.Size = new System.Drawing.Size(75, 23);
+            this.cbEstado.TabIndex = 11;
+            this.cbEstado.SelectedIndexChanged += new System.EventHandler(this.cbEstado_SelectedIndexChanged);
+            // 
+            // TelaPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MintCream;
+            this.ClientSize = new System.Drawing.Size(512, 414);
+            this.Controls.Add(this.cbEstado);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtValor);
+            this.Controls.Add(this.txtPorcento);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.KeyPreview = true;
+            this.Name = "TelaPrincipal";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Sistema de Frete...";
+            this.Load += new System.EventHandler(this.TelaPrincipal_Load);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TelaPrincipal_KeyPress);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.TextBox txtPorcento;
+        private System.Windows.Forms.TextBox txtValor;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.ComboBox cbEstado;
+    }
+}
+
---
+++ CalculoFreteEstado/TelaPrincipal.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculoFreteEstado
+{
+    public partial class TelaPrincipal : Form
+    {
+        public TelaPrincipal()
+        {
+            InitializeComponent();
+        }
+
+
+        decimal frete = 0;
+       private void CalcularFrete()
+        {
+                switch (cbEstado.Text.ToUpper())
+                {
+                case "Escolha UF":
+                    frete = 0.0m;
+                    break;
+                    case "AM":
+                        frete = 0.6m;
+                        break;
+                    case "MG":
+                        frete = 0.35m;
+                        break;
+                    case "RJ":
+                        frete = 0.30m;
+                        break;
+                    case "SP":
+                        frete = 0.20m;
+                        break;
+                    default:
+                        frete = 0.70m;
+                        break;
+                }
+            
+            // N3 é para colocar os valores em decimal com 3 casas
+
+            txtPorcento.Text = frete.ToString("P0");
+
+            //lblResultado.Text = (frete * (1 + porcentagem)).ToString("c2");
+
+        }
+
+        private void LimparTudo()
+        {
+            txtNome.Text = string.Empty;
+            txtValor.Text = string.Empty;
+            cbEstado.SelectedIndex = -1;
+            txtPorcento.Text = string.Empty;
+            lblResultado.Text = string.Empty;
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            decimal valor = 0;
+
+            
+
+            if(txtValor.Text == "")
+            {
+                MessageBox.Show("O valor está em branco");
+            }
+            else
+            {
+                valor = Convert.ToDecimal(txtValor.Text);
+                //decimal = Convert.ToDecimal(txtValor.Text = (valor * (frete + 1)).ToString();
+                lblResultado.Text = (valor * (frete + 1)).ToString("c2");
+            }
+
+
+            if (txtNome.Text.Trim().Equals(""))
+            {
+                MessageBox.Show("O campo Nome é  obrigatório", "Alerte", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                txtNome.Focus();
+                return;
+            }
+
+            if (txtValor.Text.Trim().Equals(""))
+            {
+                MessageBox.Show("O campo Valor é  obrigatório", "Alerte", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                txtValor.Focus();
+                return;
+            }
+
+           
+        }
+        private void cbEstado_SelectedIndexChanged(object sender, EventArgs e)
+        {
+          CalcularFrete();
+        }
+
+      
+
+        private void TelaPrincipal_Load(object sender, EventArgs e)
+        {
+            cbEstado.SelectedIndex = 0;        
+        }
+
+        private void TelaPrincipal_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if(e.KeyChar == 27)
+            {
+                LimparTudo();
+            }
+        }
+    }
+}
